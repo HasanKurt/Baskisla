bfb80a910ae7a0ad4eedfed9c998e30e034f925d
Hasan Kurt <hasan1989@gmail.com>
2020-11-10 07:46:19 +0100

    added some files

--- src/App.js
+++ src/App.js
@@ -1,42 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 import './Components/Persons'
 import Persons from './Components/Persons';
+import OriginalPage from './Components/OriginalPage';
+import * as data from './data.json'
+
+
 class App extends React.Component{
 
   state = {
     persons : []
+  };
+
+  loadJSON(callback) {   
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'data.json', true);
+    xobj.onreadystatechange = function () {
+      if (xobj.readyState == 4 && xobj.status == "200") {
+        callback(JSON.parse(xobj.responseText));
+      }
+    };
+    xobj.send(null); 
+  };
+
+  downloadObjectAsJson(exportObj, exportName){
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href",     dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
   }
 
   componentDidMount()
   {
-    fetch('https://localhost:44332/api/person')
-  .then(response => response.json())
-  .then((data) => this.setState({ persons: data }));
+  //   fetch('https://localhost:44332/api/person')
+  // .then(response => response.json())
+  // .then((data) => {this.setState({ persons: data }); this.downloadObjectAsJson(data, "data.json") } );
   
-  console.log('hello');
+  //use fake data
+  this.loadJSON(function(json) {
+    console.log(json); // this will log out the json object
+  });
+
+  const {allData} = data;
+  this.setState(allData);
+
   }
     
 render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-	  	       <div className="shopping-list">
-<h1>Shopping List for {this.props.name}</h1>
+      <OriginalPage />
+	  	<div className="shopping-list">
+      <h1>Shopping List for {this.props.name}</h1>
          <ul>
            <li>Instagram</li>
            <li>WhatsApp</li>
@@ -44,7 +63,7 @@ render() {
          </ul>
        </div>
 
-       <Persons persons={this.state.persons} />
+      <Persons persons={this.state.persons} />
 
     </div>
   );
--- src/Components/PersonDetails.js
+++ src/Components/PersonDetails.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 
- class PersonDetails extends Component {
-  render() {
+ const PersonDetails = () => {
     return (
         <div>
            <h1>Person Details</h1> 
         </div>
      );
-    }
-  }
+    };
+  
 
  export default PersonDetails;
