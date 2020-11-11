9042f476364ef8872d359f0b5a3df1d9bcabb779
Hasan Kurt <hasan1989@gmail.com>
2020-11-11 23:45:02 +0100

    1. Added search functionality on Persons 2. Show father id in PersonDetails, if not present show No father known msg

--- src/Components/PersonDetails.js
+++ src/Components/PersonDetails.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 
  const PersonDetails = ({match}) => {
+    
+    const [details, setDetails] = React.useState({});
+    
+    React.useEffect(()=>{
+    fetch(`https://localhost:44332/api/person/${match.params.id}`)
+    .then(response => response.json())
+    .then((data) => {console.log(data); setDetails(data);} );
+  
+    },[match.params.id])
+    
     return (
         <div>
            <h1>Person Details of {match.params.id}</h1> 
+           <h2>Lastname: {details.lastName}</h2>
+           {details.father != null && details.father != 0 ?
+           <h3>Father: {details.father}</h3>
+           :
+           <h3>Father unknown. Wanna add?</h3>
+           }
         </div>
      );
     };
--- src/Components/Persons.js
+++ src/Components/Persons.js
@@ -7,11 +7,25 @@ import {
 import PersonDetails from './PersonDetails'      
       
     const Persons = ({ persons }) => {
+
+        const [state, setState] = React.useState({searchTerm: ''})
+
+        const editSearchTerm = (event) => { setState({searchTerm: event.target.value}) }
+
+        function dynamicSearch()
+        {
+            //persons.map(person => console.log(person.lastName));
+            if(state.searchTerm == undefined)
+                return persons;
+            return persons.filter(person => (person.firstName?person.firstName:'').toLowerCase().includes(state.searchTerm))//firstName.toLowerCase().includes(state.searchTerm) )
+        }
+
       return (
         <div>
           <center><h1>Person List</h1></center>
+          <input type='text' value={state.searchTerm} onChange={editSearchTerm} placeholder="Enter a name here" />
           <>
-          {persons.map((person) => (
+          {dynamicSearch().map((person) => (
             <div>
             <Link to={`/persons/${person.id}`} ><div>{person.id} {person.firstName} {person.lastName}</div></Link>
             </div>
