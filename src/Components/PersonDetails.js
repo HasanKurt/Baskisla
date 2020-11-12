3cc55b2282b58f2a3784838cfbaefc8f796b077d
Hasan Kurt <hasan1989@gmail.com>
2020-11-12 05:38:53 +0100

    Added linking to father/mother. Added isLoading progress

--- src/Components/PersonDetails.js
+++ src/Components/PersonDetails.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom' 
 
- const PersonDetails = ({match}) => {
-    
+const PersonDetails = ({ match }) => {
+
     const [details, setDetails] = React.useState({});
-    
-    React.useEffect(()=>{
-    fetch(`https://localhost:44332/api/person/${match.params.id}`)
-    .then(response => response.json())
-    .then((data) => {console.log(data); setDetails(data);} );
-  
-    },[match.params.id])
-    
-    return (
-        <div>
-           <h1>Person Details of {match.params.id}</h1> 
-           <h2>Lastname: {details.lastName}</h2>
-           {details.father != null && details.father != 0 ?
-           <h3>Father: {details.father}</h3>
-           :
-           <h3>Father unknown. Wanna add?</h3>
-           }
-        </div>
-     );
-    };
-  
+    const [isLoading, setIsLoading] = React.useState(true);
 
- export default PersonDetails;
+    React.useEffect(() => {
+        fetch(`https://localhost:44332/api/person/${match.params.id}`)
+            .then(response => response.json())
+            .then((data) => { console.log(data); setDetails(data); setIsLoading(false); });
+
+    }, [match.params.id])
+
+    return (
+        <>
+            {
+                (isLoading || details.id == null) ? <h1>Loading..</h1>
+                    :
+                    <>
+                        <h1>Person Details of {details.firstName} {details.lastName}</h1>
+                        {
+                            details.father != null && details.father != 0 ?
+                            <Link to={`/persons/${details.father}`} ><div>Go to father of {details.firstName} </div></Link>
+                                :
+                                <h3>Father unknown. Wanna add?</h3>
+                        }
+                        {
+                            details.mother != null && details.mother != 0 ?
+                            <Link to={`/persons/${details.mother}`} ><div>Go to mother of {details.firstName} </div></Link>
+                                :
+                                <h3>Mother unknown. Wanna add?</h3>
+                        }
+                    </>
+            }
+
+
+
+        </>
+    );
+};
+
+
+export default PersonDetails;
--- src/Components/Persons.js
+++ src/Components/Persons.js
@@ -17,7 +17,10 @@ import PersonDetails from './PersonDetails'
             //persons.map(person => console.log(person.lastName));
             if(state.searchTerm == undefined)
                 return persons;
-            return persons.filter(person => (person.firstName?person.firstName:'').toLowerCase().includes(state.searchTerm))//firstName.toLowerCase().includes(state.searchTerm) )
+            return persons.filter(person => 
+                (person.firstName?person.firstName:'').toLowerCase().includes(state.searchTerm.toLowerCase())
+                || (person.lastName?person.lastName:'').toLowerCase().includes(state.searchTerm.toLowerCase())
+                )
         }
 
       return (
@@ -25,6 +28,7 @@ import PersonDetails from './PersonDetails'
           <center><h1>Person List</h1></center>
           <input type='text' value={state.searchTerm} onChange={editSearchTerm} placeholder="Enter a name here" />
           <>
+          {state.searchTerm == ''?<div></div>:<div>There are {dynamicSearch().length} people in the search results</div>}
           {dynamicSearch().map((person) => (
             <div>
             <Link to={`/persons/${person.id}`} ><div>{person.id} {person.firstName} {person.lastName}</div></Link>
